14c30ce6a6553e9a83f4826aa0b5de91ec488380
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-06 16:43:01 +0000

    Instance variable walkthrough

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,7 +8,16 @@ describe DockingStation do
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
-
   end
 
-end
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    
+    it { is_expected.to respond_to(:bike) }
+    
+    it 'returns docked bike' do
+    	bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+    end
+    
+  end
