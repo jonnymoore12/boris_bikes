c7165b157b1fe7c94767bef72b7cdc75f273dd49
acsauk <acsauk@gmail.com>
2016-07-06 15:18:39 +0100

    add test for bike working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,11 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it 'is working' do
+    bike = Bike.new
+    expect(bike).to be_working
+  end
 end
+
+ # Add a test to your DockingStation specification that
+ # a) gets a bike, and then b) expects the bike to be working
