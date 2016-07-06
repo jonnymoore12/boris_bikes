810f97453535ae62688e514c654e4a66ebf82041
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-06 19:30:15 +0100

    Adds dock method, bike getter and @bike instance variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 class DockingStation
+  # init method?
+
+  # Could have used def bike; @bike; end
+  attr_reader :bike
+
 
   def release_bike
 
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,6 +6,13 @@ describe DockingStation do
     bike = Bike.new
     expect(bike).to be_working
   end
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it 'sees the output of dock(bike)' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
 
  # Add a test to your DockingStation specification that
