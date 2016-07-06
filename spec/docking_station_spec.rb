6490ef467b48b564f6d347fd904a3e1c71c8ab2c
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-06 11:56:55 +0000

    making docking stations release bikes part 1

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,8 @@ require 'docking_station'
 describe DockingStation do
    it 'responds to request to release a bike' do
      expect(subject).to respond_to(:release_bike)
+    end
+   it 'gets a working bike' do
+   	 expect(subject).to respond_to (:Bike)
    end
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,10 +40,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4765c3208c70ce4032d86eb62ff6e2385b7e997f
+
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
