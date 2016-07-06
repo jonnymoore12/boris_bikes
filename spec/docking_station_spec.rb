29338b65e12d86b035e39327d1ab7e5bcba914c8
Bart <kluczynskib@gmail.com>
2016-07-06 15:03:25 +0200

    made a test for getting a working bike passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,11 @@ describe DockingStation do
    it 'responds to request to release a bike' do
      expect(subject).to respond_to(:release_bike)
     end
-   it 'gets a working bike' do
-   	 expect(subject).to respond_to (:Bike)
-   end
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+
+  end
+
 end
