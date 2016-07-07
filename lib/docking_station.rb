3e0453958d3f75d6f9fdbb45ed3d20d016637803
BartJudge <BartJudge@gmail.com>
2016-07-07 16:14:37 +0100

    exercise 11 using instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,14 @@
 require_relative 'bike'
-class Docking_station
+class DockingStation
+   attr_reader :bike
   def release_bike
       Bike.new
   end
+  def dock(bike)
+     @bike = bike
+  end
 
+  #def bike
+  #    @bike
+  #end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 #require 'bike'
 
 
-describe Docking_station do
+describe DockingStation do
 
 #it 'station release_bike' do
 #   expect(Docking_station).to respond_to(:release_bike)
@@ -12,6 +12,17 @@ it 'release working bikes' do
   bike = subject.release_bike
   expect(bike).to be_working
 end
-  #expect(Docking_station).to :release_bike to (Bike)
+  # expecting DockingStation instances to respond to .dock
+ #it {is_expected.to respond_to(:dock).with(1).argument }
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
 
+  #it {is_expected.to respond_to(:bike) }
+  it 'return docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
